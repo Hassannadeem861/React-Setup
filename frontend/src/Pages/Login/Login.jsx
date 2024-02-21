ae0e3ca1f0201e08f2b18c4f6d5caac24024769a
Hassan Nadeem <125460447+Hassannadeem861@users.noreply.github.com>
2024-02-21 22:23:18 +0500

    Working

--- frontend/src/Pages/Login/Login.jsx
+++ frontend/src/Pages/Login/Login.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: '',
+    password: ''
+  })
+  // console.log("user :", user);
+
+  const changeHandler = (e) => {
+    // console.log("event: ", e);
+    let name = e.target.name
+    let value = e.target.value
+
+    setUser({
+      ...user,
+      [name]: value
+    })
+  }
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("user value: ", user);
+  }
   return (
     <div>
-       <h1>Login</h1>   
+      <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "400px" }}>
+        <form onSubmit={submitHandler}>
+          <h2>Login Form</h2>
+          <label>
+            email:
+            <input
+              type="email"
+              name="email"
+              placeholder='Enter your email'
+              // value={formData.lastName}
+              // onChange={handleChange}
+              required
+              value={user.email}
+              onChange={changeHandler}
+            />
+          </label>
+          <br />
+          <label>
+            password:
+            <input
+              type="password"
+              name="password"
+              required
+              placeholder='Enter your password'
+              value={user.password}
+              onChange={changeHandler}
+
+            />
+          </label>
+
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+
     </div>
   )
 }
--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import { React, useState } from 'react'
 
 const Signup = () => {
+    const [user, setUser] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+    // console.log("user :", user);
+
+    const changeHandler = (e) => {
+        // console.log("event: ", e);
+        let name = e.target.name
+        let value = e.target.value
+
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log("user value: ", user);
+    }
+
+
+
     return (
-        <div>
-            <h1>Signup</h1>
+        <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "400px" }}>
+            <form onSubmit={submitHandler}>
+                <h2>Registration Form</h2>
+                <label>
+                    username:
+                    <input
+                        type="text"
+                        name="username"
+                        // value={formData.firstName}
+                        // onChange={handleChange}
+                        required
+                        placeholder='Enter your username'
+                        value={user.username}
+                        onChange={changeHandler}
+                    />
+                </label>
+                <br />
+                <label>
+                    email:
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder='Enter your email'
+                        // value={formData.lastName}
+                        // onChange={handleChange}
+                        required
+                        value={user.email}
+                        onChange={changeHandler}
+                    />
+                </label>
+                <br />
+                <label>
+                    password:
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        placeholder='Enter your password'
+                        value={user.password}
+                        onChange={changeHandler}
+
+                    />
+                </label>
+
+                <br />
+                <button type="submit">Register</button>
+            </form>
         </div>
     )
 }
