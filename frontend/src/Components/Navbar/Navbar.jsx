2be9decc6b6be9f52c8a91922d6ebd9d73514716
Hassan Nadeem <125460447+Hassannadeem861@users.noreply.github.com>
2024-02-21 17:27:13 +0500

    Working

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -5,34 +5,34 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='container'>
-        <div className='logo' style={{width: "50px", height: "50px", backgroundColor: "red"}}>
-          <img src='' alt=''></img>
-        </div>
+            <div className='logo' style={{ width: "50px", height: "50px", backgroundColor: "red" }}>
+                <img src='' alt=''></img>
+            </div>
             <nav>
                 <ul className='flex'>
-                    <li className=''>
-                        <Link to="/">Home</Link>
+                    <li>
+                        <Link to="/" style={{color: 'white', textDecorationLine: "none"}}>Home</Link>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <Link to="/about" style={{color: 'white', textDecorationLine: "none"}}>About</Link>
                     </li>
                     <li>
-                        <Link to="/contact">Contact</Link>
+                        <Link to="/contact" style={{color: 'white', textDecorationLine: "none"}}>Contact</Link>
                     </li>
                     <li>
-                        <Link to="/services">Services</Link>
+                        <Link to="/services" style={{color: 'white', textDecorationLine: "none"}}>Services</Link>
                     </li>
                     <li>
-                        <Link to="/login">Login</Link>
+                        <Link to="/signup" style={{color: 'white', textDecorationLine: "none"}}>Signup</Link>
                     </li>
                     <li>
-                        <Link to="/signup">Signup</Link>
+                        <Link to="/login" style={{color: 'white', textDecorationLine: "none"}}>Login</Link>
                     </li>
                 </ul>
             </nav>
 
             <div>
-                <input type='search' required placeholder='Enter your search'></input>
+                <input type='search' required placeholder='Enter your search' style={{width: '300px', height: "40px"}}></input>
             </div>
 
         </div>
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import {React, useEffect} from 'react'
+import './Home.css'
+import video from '../../video/video.mp4'
+
 
 const Home = () => {
+
+  useEffect(() => {
+    const video = document.getElementById('background-video');
+    video.playbackRate = 0.5; // Adjust playback speed if needed
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="video-background">
+      <video autoPlay muted loop id="background-video">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="content">
+      
+      </div>
     </div>
   )
 }
